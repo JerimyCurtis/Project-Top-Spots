be09685ab7d877e613232116c09b2ce8eb84dcb1
oxbojyd <153137143+oxbojyd@users.noreply.github.com>
2024-01-02 22:44:45 +0100

    First draft, no html display

--- main.js
+++ main.js
@@ -1,4 +1,22 @@
 $(document).ready(function() {
-// write your code here
-
+$.getJSON('data.json', function(data){
+    createHTML(data);
+})
+.fail(function(error) {
+    console.error('Error reading JSON file:', error);
+});
+function createHTML(data){
+    var container = $('#dynamic-content');
+    var table = $('<table>');
+    var headerRow = $('<tr>');
+    headerRow.append($('<th>').text('Name'));
+    
+    table.append(headerRow);
+    
+    $.each(data, function(index, item){
+        var row = $('<tr>');
+        row.append($('<td>').text(item.name));
+        table.append(row);
+    })
+}
 });
