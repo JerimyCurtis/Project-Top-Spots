4ba663aee0bb70ac2d411e8b2502628cdbc75aa6
oxbojyd <153137143+oxbojyd@users.noreply.github.com>
2024-01-03 00:36:50 +0100

    Displays, created css

--- main.js
+++ main.js
@@ -1,22 +1,30 @@
 $(document).ready(function() {
-$.getJSON('data.json', function(data){
-    createHTML(data);
-})
-.fail(function(error) {
-    console.error('Error reading JSON file:', error);
-});
-function createHTML(data){
-    var container = $('#dynamic-content');
-    var table = $('<table>');
-    var headerRow = $('<tr>');
-    headerRow.append($('<th>').text('Name'));
-    
-    table.append(headerRow);
-    
-    $.each(data, function(index, item){
-        var row = $('<tr>');
-        row.append($('<td>').text(item.name));
-        table.append(row);
+    $.getJSON('data.json', function(data) {
+        createHTML(data);
     })
-}
+    .fail(function(error) {
+        console.error('Error reading JSON file:', error);
+    });
+
+    function createHTML(data) {
+        var container = $('#dynamic-content');
+        var table = $('<table>');
+        var headerRow = $('<tr>');
+        headerRow.append($('<th>').text('Name'));
+        headerRow.append($('<th>').text('Description'));
+        headerRow.append($('<th>').text('Location'));
+        
+        table.append(headerRow);
+
+        $.each(data, function(index, item) {
+            var row = $('<tr>');
+            row.append($('<td>').text(item.name));
+            row.append($('<td>').text(item.description));
+            row.append($('<td>').text('Latitude: ' + item.location[0] + ', Longitude: ' + item.location[1]));
+
+            table.append(row);
+        });
+
+        container.append(table);
+    }
 });
